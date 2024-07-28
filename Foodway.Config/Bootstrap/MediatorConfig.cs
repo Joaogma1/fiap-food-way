0bb749eb66993beadab0c50241c97471f273c33e
Joao Gabriel Mauricio <joao.mauricio@hatchworks.com>
2024-07-28 19:07:22 -0300

    refactor(config): added reflection to simplify useCases IoC

--- Foodway.Config/Bootstrap/MediatorConfig.cs
+++ Foodway.Config/Bootstrap/MediatorConfig.cs
@@ -1,8 +1,5 @@
 using System.Reflection;
 using Foodway.Application.UseCases.Auth.Commands.SignInCommand;
-using Foodway.Application.UseCases.Client.Commands.CreateClientCommand;
-using Foodway.Application.UseCases.Order.Commands.CreateOrderCommand;
-using Foodway.Application.UseCases.Product.Commands.CreateProductCommand;
 using Foodway.Config.Pipelines;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,16 +10,19 @@ public static class MediatorConfig
 {
     public static IServiceCollection AddMediator(this IServiceCollection services)
     {
+        var useCaseAssembly = typeof(SignInCommandHandler).Assembly.GetTypes()
+            .Where(type => type is {IsClass: true, IsAbstract: false}
+                           && type.Name.EndsWith("Handler")
+                           && type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)))
+            .Select(x => x.GetTypeInfo().Assembly).First();
+
         services.AddMediatR(cfg =>
         {
-            cfg.RegisterServicesFromAssemblies(typeof(SignInCommandHandler).GetTypeInfo().Assembly);
-            cfg.RegisterServicesFromAssemblies(typeof(CreateClientCommandHandler).GetTypeInfo().Assembly);
-            cfg.RegisterServicesFromAssemblies(typeof(CreateProductCommandHandler).GetTypeInfo().Assembly);
-            cfg.RegisterServicesFromAssemblies(typeof(CreateOrderCommandHandler).GetTypeInfo().Assembly);
+            cfg.RegisterServicesFromAssembly(useCaseAssembly);
             services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
             cfg.AddOpenBehavior(typeof(ValidatorBehavior<,>));
         });
-        
+
         return services;
     }
 }
